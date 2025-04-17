7683fab93bfb08917fed54d685622ea077a3b07f
ml <88625636+lleomllr@users.noreply.github.com>
2025-04-17 12:05:03 +0200

    Update note.model.ts

--- src/app/note.model.ts
+++ src/app/note.model.ts
@@ -1,26 +1,10 @@
 import { Tag } from './tag';
 
 export class Notes {
-  private static lastId = 0;
-
-  id: number;
-  title: string;
-  tags: Tag[];
-  content: string;
-
-  constructor(title: string, content: string = '', tags: Tag[] = [], id?: number) {
-    this.title = title;
-    this.content = content;
-    this.tags = tags;
-
-    if (id !== undefined) {
-      this.id = id;
-      if (id > Notes.lastId) {
-        Notes.lastId = id;
-      }
-    } else {
-      Notes.lastId++;
-      this.id = Notes.lastId;
-    }
-  }
+  constructor(
+    public title: string,
+    public content: string,
+    public tags: Tag[] = [],
+    public id: number = 0
+  ) {}
 }
